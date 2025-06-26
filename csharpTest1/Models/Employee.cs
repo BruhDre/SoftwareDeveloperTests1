42aa2d4a06eba54c30c919fa2e0651906f703521
BruhDre <134733574+BruhDre@users.noreply.github.com>
2025-06-26 10:35:13 +0200

    Add files via upload

---
+++ csharpTest1/Models/Employee.cs
@@ -0,0 +1,7 @@
+namespace CSharpTest1.Models {
+    public class Employee
+    {
+        public string EmployeeName { get; set; } = "";
+        public double TotalTimeWorked { get; set; } = 0;
+    }
+}
